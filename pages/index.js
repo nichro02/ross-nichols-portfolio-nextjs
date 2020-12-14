2fa539ffdbd111fc3c96b7cb8c03f789f336bc59
nichro02 <nichro02@gmail.com>
2020-12-13 21:54:01 -0800

    Wrote API call to retrive Github repos to post on projects page

--- pages/index.js
+++ pages/index.js
@@ -39,16 +39,6 @@ export default function Home() {
             <h3>Contact &rarr;</h3>
             <p>Get in touch</p>
           </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
--- pages/projects.js
+++ pages/projects.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 //components
 //import Spinner from './common/Spinner'
 
 const Projects = () => {
+    //initialize state to be used when fetching repos
+    const [data, setData] = useState([])
+    //initialize state for loader
+    const [loading, setLoading] = useState(true)
+
+    //retrieve Github repos amd set state
+    useEffect(() => {
+        axios.get('https://api.github.com/users/nichro02/repos').then((res) => {
+          console.log(res.data)
+          setData(res.data)
+        }).then(() => setLoading(false))
+      }, [])
+
+      const display = () => {
+    
+        if(loading) {
+          {/*return <Spinner />*/}
+          return <h1>Page Loading</h1>
+        } else {
+          return data.map((card) => (
+          
+            <div key={card.id} className="col s12 m6">
+              <div className="card blue-grey darken-1">
+                <div className="card-content white-text">
+                  <span className="card-title">{card.name}</span>
+                  <p>I am a very simple card. I am good at containing small bits of information.
+                  I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div className="card-action">
+                  <a href="#">This is a link</a>
+                  <a href="#">This is a link</a>
+                </div>
+              </div>
+            </div>
+          
+          ))
+        }
+      }  
+
+    
     return(
+        <>
         <div>Projects go here</div>
+        <div className="container">
+        <div className="row">{display()}</div>
+        </div>
+        </>
     )
 }
 
