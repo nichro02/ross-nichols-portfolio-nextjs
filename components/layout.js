a12b2448b856316e1f3ca6895cd7245de7b1c80e
nichro02 <nichro02@gmail.com>
2020-12-18 14:50:52 -0800

    Fixed footer on layout component to bottom of page

--- components/layout.js
+++ components/layout.js
@@ -30,19 +30,36 @@ const Layout = ({ children }) => {
                     </span>
                     </span>
                     </div>
-                    <div className={styles.introBlock}>
+                    {/*<div className={styles.introBlock}>
                         <h1 className={styles.greetingText}>Ross Nichols </h1>
                         <h2 className={styles.introText}>
                         Software Engineer
                         </h2>
-                    </div>
+                    </div>*/}
                     
                 </div>
             </nav>
             <div className={styles.container}>{children}</div>
-            <footer className={styles.navBar}>
-                <div>
-                    <ul>
+            <footer className={styles.footer}>
+                <div className={styles.footerDiv}>
+                <span className={styles.footerLinks}>
+                    <span className={styles.link}>
+                    <Link href='/about'>
+                        About
+                    </Link>
+                    </span>
+                    <span className={styles.link}>
+                    <Link href='/projects'>
+                        Projects
+                    </Link>
+                    </span>
+                    <span className={styles.link}>
+                    <Link href='/contact'>
+                        Contact
+                    </Link>
+                    </span>
+                    </span>
+                   {/* <ul>
                     <Link href='/'>
                         Home
                     </Link>
@@ -55,8 +72,8 @@ const Layout = ({ children }) => {
                     <Link href='/contact'>
                         Contact
                     </Link>
-                    </ul>
-                    <h6>Website created by Ross Nichols</h6>
+                    </ul> */}
+                    <h6 className={styles.footerText}>Website created by Ross Nichols</h6>
                 </div>
             </footer>
         </>
